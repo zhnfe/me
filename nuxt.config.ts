fe591237668cdd9ef500de360cf254d0eb57c441
haonan <work.haonan@gmail.com>
2024-07-23 17:45:45 +0800

    关闭内联样式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  features: {
+    inlineStyles: false
+  },
   app: {
     head: {
       viewport: 'width=device-width, initial-scale=1, user-scalable=1',
